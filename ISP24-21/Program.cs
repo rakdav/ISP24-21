24dc4e7e194844389a548867ea5a67741adf7557
rakdav <rakdav@inbox.ru>
2025-09-26 14:01:05 +0200

    Добавьте файлы проекта.

---
+++ ISP24-21/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите ваше имя:");
+string name=Console.ReadLine();
+Console.WriteLine("Ваше имя "+name);
+Console.WriteLine("Ваше имя {0}",name);
+Console.WriteLine($"Ваше имя {name}");
---
+++ Lab1/Program.cs
@@ -0,0 +1,86 @@
+﻿//Console.Write("Введите x:");
+//double x=double.Parse(Console.ReadLine());
+////double x=Convert.ToDouble(Console.ReadLine());
+//double y = x + 2;
+//Console.WriteLine($"y={y}");
+//sbyte a = -121;
+//double b = 9.0;
+//float c = 8.0f;
+//decimal money = 100.5m;
+//double e = a + b;
+//byte f = (byte)220.0;
+//short l = 128;
+//sbyte s = (sbyte)l;
+//Console.WriteLine(b);
+//Console.WriteLine(s);
+//int x;
+//Console.WriteLine($"{Math.PI:f2}");
+//Console.WriteLine($"{Math.E:f1}");
+//Console.WriteLine($"{1} {13} {49}");
+//Console.WriteLine($"{7}\t{15}\t{100}");
+//Console.WriteLine($"{50}\n{10}");
+//Console.WriteLine($"{5} {10}\n{7} cm");
+
+//Console.Write("Введите x:");
+//double x=double.Parse(Console.ReadLine());
+//Console.WriteLine($"{2*x:f2}");
+//Console.WriteLine($"{Math.Sin(x):f2}");
+//Console.WriteLine($"{x*x}");
+//Console.WriteLine($"{Math.Sqrt(x):f2}");
+//Console.WriteLine($"{Math.Abs(x)}");
+//Console.WriteLine($"{5*Math.Cos(x):f2}");
+
+//Console.Write("Введите a:");
+//double a=double.Parse(Console.ReadLine());
+
+//Console.Write("Введите b:");
+//double b = double.Parse(Console.ReadLine());
+
+//double y=Math.Sin(a*Math.PI/180)*Math.Cos(b * Math.PI / 180)+Math.Cos(a * Math.PI / 180)* Math.Sin(b * Math.PI / 180);
+//Console.WriteLine($"y={y:f2}");
+
+
+//Console.Write("Введите x:");
+//double x = double.Parse(Console.ReadLine());
+
+//Console.Write("Введите y:");
+//double y = double.Parse(Console.ReadLine());
+
+//double z = -5 * Math.Sqrt(x +Math.Sqrt(y));
+//Console.WriteLine($"z={z:f2}");
+//Console.WriteLine($"z={-1/(x*x):f2}");
+
+//Console.Write("Введите a:");
+//double a = double.Parse(Console.ReadLine());
+
+//Console.Write("Введите b:");
+//double b = double.Parse(Console.ReadLine());
+
+//Console.WriteLine($"z={5.45*(a+2*b)/(2-a):f2}");
+
+//Console.WriteLine($"z={1/(1+1/(2+1/(2+3.0/5))):f2}");
+
+//лабораторная работа #1
+//Арифметические выражения
+//Вариант 30. Базовый
+//Console.Write("Введите y:");
+//double y = double.Parse(Console.ReadLine());
+//Console.Write("Введите r:");
+//double r = double.Parse(Console.ReadLine());
+//Console.WriteLine($"z={Math.Exp(y+r)+7.2*Math.Sin(r*Math.PI/180):f2}");
+
+//Вариант 30. Средний
+//Console.Write("Введите y:");
+//double y = double.Parse(Console.ReadLine());
+//Console.Write("Введите r:");
+//double p = double.Parse(Console.ReadLine());
+//Console.WriteLine($"N={(3*y*y+Math.Sqrt(y+1))/(Math.Log(p+y)+Math.Exp(p)):f2}");
+
+//Вариант 30. Высокий
+//Console.Write("Введите x:");
+//double x = double.Parse(Console.ReadLine());
+//Console.WriteLine($"K={(Math.Sqrt(Math.Pow(3+x,6)-Math.Log(x)))/(Math.Exp(0)+Math.Asin(6*x*x)):f2}");
+
+//лабораторная работа #2
+//Программирование линейных алгоритмов
+//Вариант 30. Базовый
