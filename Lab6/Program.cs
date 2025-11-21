6ed93891457c6fa28c02f2ffaa53da4abde894aa
rakdav <rakdav@inbox.ru>
2025-11-21 11:20:38 +0200

    Lab6

--- Lab6/Program.cs
+++ Lab6/Program.cs
@@ -5,108 +5,224 @@ using System.Xml.Linq;
 //{
 //    return a + b;
 //}
-int Sum(int a, int b)=>a+b;
-void Swap(ref int a,ref int b)
+//int Sum(int a, int b)=>a+b;
+//void Swap(ref int a,ref int b)
+//{
+//    int temp=a; 
+//    a=b; 
+//    b=temp;
+//}
+//double Area(double R)
+//{
+//    return Math.PI*R*R;
+//}
+//long Factorial(int n)
+//{
+//    long F = 1;
+//    for (int i = 1; i <=n; i++)  F*= i;
+//    return F;
+//}
+//void getHello()
+//{
+//    Console.WriteLine("Hello, world!");
+//}
+//void getName(string name)
+//{
+//    Console.WriteLine($"Привет! Меня зовут {name}");
+//}
+//void getDate()=> Console.WriteLine($"{DateTime.Now}");
+//void Summa(double a, double b)=> Console.WriteLine($"{a}+{b}={a+b}");
+////Необязательные параметры
+//void printPerson(string name,int age=1,string company = "Undefined")
+//{
+//    Console.WriteLine($"Name:{name} Age:{age} Company:{company}");
+//}
+//void SumOut(int x,int y,out int result)
+//{
+//    result = x+y;
+//}
+
+//void getRectangleData(in int w,in int h,out int area,out int perimetr)
+//{
+//   // w = 7;
+//    area = w * h;
+//    perimetr = (w+h)*2;
+//}
+//void Increment(ref readonly int n)
+//{
+//    //n++;
+//    Console.WriteLine(n);
+//}
+//void sumNumbers(params int[] numbers)
+//{
+//    int sum = 0;
+//    foreach(var n in numbers) sum+= n;
+//    Console.WriteLine(sum);
+//}
+//int sumMas(int[] mas)
+//{
+//    mas[0] = 78;
+//    int s = 0;
+//    foreach (var n in mas) s+= n;
+//    return s;
+//}
+//try
+//{
+//    Console.Write("Введите первую переменную:");
+//int x = int.Parse(Console.ReadLine());
+//Console.Write("Введите вторую переменную:");
+//int y = int.Parse(Console.ReadLine());
+//Console.WriteLine($"{x}+{y}={Sum(x,y)}");
+//Console.Write("Введите радиус окружности:");
+//double r = double.Parse(Console.ReadLine());
+//Console.WriteLine($"S={Area(r):F2}");
+//Console.WriteLine($"{x}!={Factorial(x)}");
+// getHello();
+//Console.Write("Введите имя:");
+//string name=Console.ReadLine();
+////getName(name);
+//getDate();
+//Summa(x, y);
+//printPerson("Иван");
+//printPerson("Вася",60);
+//printPerson("Tom", 40, "Microsoft");
+//printPerson("Bob", company:"Samsung",age:34);
+//Console.WriteLine($"x={x} y={y}");
+//Swap(ref x,ref y);
+//Console.WriteLine($"x={x} y={y}");
+//int res;
+//SumOut(x,y,out res);
+//Console.WriteLine($"{x}+{y}={res}");
+//Console.Write("Введите число:");
+//string n=Console.ReadLine();
+//if (int.TryParse(n, out var number)) Console.WriteLine(number);
+//double v = number * 2;
+//int area, perimetr;
+//getRectangleData(x, y,out area,out perimetr);
+//Console.WriteLine(area);
+//Console.WriteLine(perimetr);
+//    int[] mas = { 1, 4, 5, 67 };
+//    sumNumbers(mas);
+//    sumNumbers(1,5,7,8,59);
+//    Console.WriteLine(sumMas(mas));
+//}
+//catch(Exception ex)
+//{
+//    Console.WriteLine(ex.Message);
+//}
+
+//Реализация сортировки слиянием
+//метод для слияния массивов
+void Merge(int[] a, int l, int m, int r)
 {
-    int temp=a; 
-    a=b; 
-    b=temp;
-}
-double Area(double R)
+    int i, j, k;
+
+int n1 = m - l + 1;
+int n2 = r - m;
+
+int[] left = new int[n1 + 1];
+int[] right = new int[n2 + 1];
+
+for (i = 0; i < n1; i++)
 {
-    return Math.PI*R*R;
-}
-long Factorial(int n)
-{
-    long F = 1;
-    for (int i = 1; i <=n; i++)  F*= i;
-    return F;
-}
-void getHello()
-{
-    Console.WriteLine("Hello, world!");
-}
-void getName(string name)
-{
-    Console.WriteLine($"Привет! Меня зовут {name}");
-}
-void getDate()=> Console.WriteLine($"{DateTime.Now}");
-void Summa(double a, double b)=> Console.WriteLine($"{a}+{b}={a+b}");
-//Необязательные параметры
-void printPerson(string name,int age=1,string company = "Undefined")
-{
-    Console.WriteLine($"Name:{name} Age:{age} Company:{company}");
-}
-void SumOut(int x,int y,out int result)
-{
-    result = x+y;
+    left[i] = a[l + i];
 }
 
-void getRectangleData(in int w,in int h,out int area,out int perimetr)
+for (j = 1; j <= n2; j++)
 {
-   // w = 7;
-    area = w * h;
-    perimetr = (w+h)*2;
+    right[j - 1] = a[m + j];
 }
-void Increment(ref readonly int n)
+
+left[n1] = int.MaxValue;
+right[n2] = int.MaxValue;
+
+i = 0;
+j = 0;
+
+for (k = l; k <= r; k++)
 {
-    //n++;
-    Console.WriteLine(n);
+    if (left[i] < right[j])
+    {
+        a[k] = left[i];
+        i = i + 1;
+    }
+    else
+    {
+        a[k] = right[j];
+        j = j + 1;
+    }
 }
-void sumNumbers(params int[] numbers)
-{
-    int sum = 0;
-    foreach(var n in numbers) sum+= n;
-    Console.WriteLine(sum);
 }
-int sumMas(int[] mas)
+//сортировка слиянием
+int[] MergeSort(int[] array,int lowIndex,int hightIndex)
 {
-    mas[0] = 78;
-    int s = 0;
-    foreach (var n in mas) s+= n;
-    return s;
+    if (lowIndex < hightIndex)
+    {
+        int middleIndex = (lowIndex + hightIndex) / 2;
+        MergeSort(array, lowIndex, middleIndex);
+        MergeSort(array, middleIndex+1, hightIndex);
+        Merge(array, lowIndex, middleIndex, hightIndex);
+    }
+    return array;
 }
-try
+
+int[] MergeSortAlg(int[] array)
 {
-    //    Console.Write("Введите первую переменную:");
-    //int x = int.Parse(Console.ReadLine());
-    //Console.Write("Введите вторую переменную:");
-    //int y = int.Parse(Console.ReadLine());
-    //Console.WriteLine($"{x}+{y}={Sum(x,y)}");
-    //Console.Write("Введите радиус окружности:");
-    //double r = double.Parse(Console.ReadLine());
-    //Console.WriteLine($"S={Area(r):F2}");
-    //Console.WriteLine($"{x}!={Factorial(x)}");
-    // getHello();
-    //Console.Write("Введите имя:");
-    //string name=Console.ReadLine();
-    ////getName(name);
-    //getDate();
-    //Summa(x, y);
-    //printPerson("Иван");
-    //printPerson("Вася",60);
-    //printPerson("Tom", 40, "Microsoft");
-    //printPerson("Bob", company:"Samsung",age:34);
-    //Console.WriteLine($"x={x} y={y}");
-    //Swap(ref x,ref y);
-    //Console.WriteLine($"x={x} y={y}");
-    //int res;
-    //SumOut(x,y,out res);
-    //Console.WriteLine($"{x}+{y}={res}");
-    //Console.Write("Введите число:");
-    //string n=Console.ReadLine();
-    //if (int.TryParse(n, out var number)) Console.WriteLine(number);
-    //double v = number * 2;
-    //int area, perimetr;
-    //getRectangleData(x, y,out area,out perimetr);
-    //Console.WriteLine(area);
-    //Console.WriteLine(perimetr);
-    int[] mas = { 1, 4, 5, 67 };
-    sumNumbers(mas);
-    sumNumbers(1,5,7,8,59);
-    Console.WriteLine(sumMas(mas));
+    return MergeSort(array, 0, array.Length-1);
 }
-catch(Exception ex)
+Random random=new Random();
+int[] mas = new int[20];
+for (int i = 0; i < mas.Length; i++)
 {
-    Console.WriteLine(ex.Message);
+    mas[i] = random.Next(1, 100);
+}
+//Console.WriteLine(string.Join(", ", mas));
+//Console.WriteLine();
+//Console.WriteLine(string.Join(", ", MergeSortAlg(mas)));
+
+//Базовый уровень. 1 вариант.
+Console.WriteLine($"{Func(5)+Func(12)+Func(19):F2}");
+//Средний уровень. 1 вариант.
+Console.WriteLine(ToBinary(9));
+//Высокий уровень. 1 вариант.
+Console.WriteLine(Fact(5));
+Console.WriteLine(Fact2(6));
+Console.WriteLine(Fact2(7));
+
+double Func(int i)
+{
+    return (Math.Sqrt(i)+i) / 2;
+}
+string ToBinary(int n)
+{
+    string res = "";
+    while (n != 1)
+    {
+        int remainder =n % 2;
+        res += remainder;
+        n /= 2;
+    }
+    res += "1";
+    char[] chars = res.ToCharArray();
+    Array.Reverse(chars);
+    return new string(chars);
+}
+
+long Fact(int n)
+{
+    if (n == 0 || n == 1) return 1;
+    else return Fact(n - 1) * n;
+}
+long Fact2(int n)
+{
+    if (n % 2 == 0)
+    {
+        if (n == 2) return 2;
+        else return Fact2(n-2)*n;
+    }
+    else
+    {
+        if (n == 1) return 1;
+        else return Fact2(n - 2) * n;
+    }
 }
